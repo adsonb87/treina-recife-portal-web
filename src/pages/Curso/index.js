ab20d3f855d9132fa8d4a901e057ff4668234a8f
adsonb87 <adsonb87@gmail.com>
2023-08-07 20:31:48 -0300

    Implementado pagina de cursos detalhado

--- src/pages/Curso/index.js
+++ src/pages/Curso/index.js
@@ -1,57 +1,60 @@
-import { useEffect, useState } from "react";
 import Cabecalho from "../../components/Cabecalho";
 import Footer from "../../components/Footer";
-import { useCursos } from "../../hooks/useCursos";
-
+import { useState } from "react";
 import { LISTA_CURSOS } from "../../mocks/cursos";
-import CardDetail from "../../components/CardDetail";
 
-const Curso = () => {
-    
-    const [curso, setCurso] = useCursos();
-    const [busca, setBusca] = useState(0);
+function Curso() {
+  const [curso] = useState(LISTA_CURSOS[0]);
 
-    useEffect(() => {
-        const cursoFiltrado = LISTA_CURSOS.filter((curso) => curso.id === parseInt(busca));
-        
-        setCurso(cursoFiltrado);
-        
-    }, [busca]);
+  return (
+    <>
+      <Cabecalho />
 
-    return (
-        <>
-            <Cabecalho />
-            <div className="col-md-5 container">
-                <input 
-                    type="number"
-                    className="form-control mb-3 mt-3"
-                    placeholder="Filtrar curso pelo id..."
-                    min={0}
-                    max={9}
-                    onChange={(event) => setBusca(event.target.value)}
-                />
-                    {
-                        curso.map((c, i) => {
-                            return(
-                                <CardDetail 
-                                    key={i}
-                                    linkImg={c.imagem}
-                                    titulo={c.nome}
-                                    descricao={c.descricao}
-                                    cargaHoraria={c.cargaHoraria}
-                                    preco={c.preco}
-                                    nivel={c.nivel}
-                                    professor={c.professor}
-                                    ementa={c.ementa}
-                                />
-                            );
-                        })
-                    }
-            </div>
-            <Footer />
-        </>
-    );
+      <div className="bg-body-secondary p-5 mb-4">
+        <div className="container-fluid py-5">
+          <h1 className="display-5 fw-bold">{curso.nome}</h1>
+          <p className="col-md-6 fs-4">{curso.descricao}</p>
+          <p className="col-md-6 fs-5">
+            <b>Nível: </b>
+            {curso.nivel} - <b>Investimento: </b>R$ {curso.preco}
+          </p>
+          <button className="btn btn-primary btn-lg" type="button">
+            Comprar
+          </button>
+        </div>
+      </div>
+
+      <div className="list-group p-5">
+        <h3 className="fw-600">Ementa</h3>
+        <ol>
+          {curso.ementa.map((item, indice) => (
+            <li key={indice}>{item}</li>
+          ))}
+        </ol>
+      </div>
+
+      <div className="bg-body-secondary p-5">
+        <img
+          className="mb-3"
+          src={curso.professor.imagem}
+          alt={curso.professor.nome}
+          width="250px"
+        />
+        <h3 className="fw-600">Professor: {curso.professor.nome}</h3>
+        <p className="col-md-8">{curso.professor.resumo}</p>
+        <a
+          className="btn btn-primary"
+          href={curso.professor.linkedin}
+          target="_blank"
+          rel="noreferrer"
+        >
+          Ver Linkedin &raquo;
+        </a>
+      </div>
+
+      <Footer />
+    </>
+  );
 }
-    
 
 export default Curso;
