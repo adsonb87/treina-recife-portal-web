0dd4358b9d36530a019168374107958c4d4830a9
adsonb87 <adsonb87@gmail.com>
2023-08-14 21:33:26 -0300

    Criar CRUD e Login

--- src/services/api.js
+++ src/services/api.js
@@ -15,3 +15,13 @@ export async function listarUsuarios(setUsuarios) {
 export async function deletarUsuario(indice){
     await api.delete(`/usuarios/${indice}`).catch((error) => console.error(error));
 }
+
+
+/**
+ * TODO:
+ * Criar tela de usuario (cadastro e edição)
+ * criar as funções de cadastrar e atualizar
+ * criar uma funcao de login
+ * ao fazer login, salvar o usuario logado em um estado global usuarioLogado
+ * armazenamento local
+ */
